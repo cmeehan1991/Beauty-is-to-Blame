b2290d7656edf10fb72694d6fdf3d8a2f8caa690
Connor Meehan <cmeehan@elon.edu>
2021-11-04 14:24:44 +0000

    Moving stuff around.

--- assets/js/global/navbar.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-	$('a.nav-link[title=Cart]').html('<i class="fa fa-shopping-cart" aria-hidden="true"></i>');
-});
--- assets/js/global/post.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-	$('.next-post-link, .previous-post-link').hover(function(){
-		$(this).popover('toggle');
-	});
-});
