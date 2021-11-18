0db5e693feedda586ba12c3aa7e17c1a89354d80
Connor Meehan <cmeehan@elon.edu>
2021-11-18 19:46:42 +0000

    Updating SEO meta

--- partials/head-meta.php
+++ partials/head-meta.php
@@ -1,11 +1,29 @@
+<?php 
+$meta_title = wp_title() ? wp_title() : get_bloginfo('title');
+
+$meta_url = get_the_permalink() ? get_the_permalink() : get_bloginfo('url');
+
+$meta_description = get_bloginfo('description');
+
+if(get_post_meta(get_the_ID(), '_yoast_wpseo_metadesc', true)){
+	$meta_description = get_post_meta(get_the_ID(), '_yoast_wpseo_metadesc', true)
+}elseif(get_the_excerpt() && !is_home()){
+	$meta_description = get_the_excerpt();
+}
+
+$meta_image = get_the_post_thumbnail_url() ? get_the_post_thumbnail_url() : get_header_image();
+
+?>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="Hi everyone! Welcome to my blog, Beauty is to Blame. My name is Taylor Meehan. On my blog, I aim to share my personal style, as well as, general style advice, my favorite beauty products, some home decor from time to time, and other things pertaining to lifestyle. My aspiration is to inspire/influence all of you in a positive way and for my blog to be a helpful, informative, and enjoyable place for you all to delve into.">
-<meta name="keywords" content="blog,blogger,fashion blog, fashion blogger, fashion, style, style blogger, style blog, nc, burlington, triad, triangle, raleigh-durham, raleigh, beauty, makeup, jewelry.">
+<meta name="robots" content="index,follow">
+<meta name="description" content="<?php echo $meta_description; ?>">
 
-<meta property="og:url" content="http://www.beautyistoblame.com">
-<meta property="og:description" content="Hi everyone! Welcome to my blog, Beauty is to Blame. My name is Taylor Meehan. On my blog, I aim to share my personal style, as well as, general style advice, my favorite beauty products, some home decor from time to time, and other things pertaining to lifestyle. My aspiration is to inspire/influence all of you in a positive way and for my blog to be a helpful, informative, and enjoyable place for you all to delve into.">
-<meta property="og:image" content="<?php echo get_header_image(); ?>">
+
+<!--Facebook OG tags-->	
+<meta property="og:url" content="<?php echo $meta_url; ?>">
+<meta property="og:description" content="<?php echo $meta_description; ?>">
+<meta property="og:image" content="<?php echo $meta_image; ?>">
 <meta property="gb:app_id" content="431068527368139">
 
 
